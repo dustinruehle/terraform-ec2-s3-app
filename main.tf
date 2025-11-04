f4187b144bdf6f0964538edb7578566976c3b98c
Dustin Ruehle <dustin.ruehle@temporal.io>
2025-11-03 23:13:49 -0700

    adding ssh keypair to EC2

--- main.tf
+++ main.tf
@@ -213,6 +213,7 @@ resource "aws_instance" "web_server" {
   instance_type          = var.instance_type
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [aws_security_group.web_server.id]
+  key_name               = "temporal-demo-key"
 
   user_data = templatefile("${path.module}/user-data.sh", {
     bucket_name = aws_s3_bucket.content.id
